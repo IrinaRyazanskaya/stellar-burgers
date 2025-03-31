e5a0cbdf048ad0481559f7c8e68b85dee8cd888b
Irina Ryazanskaya <ryazanskaya.irina@yandex.com>
2025-03-31 21:46:19 +0200

    Сделала роуты анонимными

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,13 @@
 import { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
-import { AppHeader, Modal, OrderInfo, IngredientDetails } from '@components';
+import {
+  AppHeader,
+  OrderInfo,
+  IngredientDetails,
+  ProtectedRoute,
+  AnonymousRoute
+} from '@components';
 import { fetchBurgerIngredients, getUser } from '@slices';
 import {
   ConstructorPage,
@@ -18,7 +24,6 @@ import { useDispatch } from '../../services/store';
 
 import '../../index.css';
 import styles from './app.module.css';
-import { ProtectedRoute } from '../protected-route';
 import { useRedirectOnLogout } from '../../utils/redirects';
 
 const App = () => {
@@ -42,10 +47,38 @@ const App = () => {
       <Routes>
         <Route path='/' element={<ConstructorPage />} />
         <Route path='/feed' element={<Feed />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route
+          path='/login'
+          element={
+            <AnonymousRoute>
+              <Login />
+            </AnonymousRoute>
+          }
+        />
+        <Route
+          path='/register'
+          element={
+            <AnonymousRoute>
+              <Register />
+            </AnonymousRoute>
+          }
+        />
+        <Route
+          path='/forgot-password'
+          element={
+            <AnonymousRoute>
+              <ForgotPassword />
+            </AnonymousRoute>
+          }
+        />
+        <Route
+          path='/reset-password'
+          element={
+            <AnonymousRoute>
+              <ResetPassword />
+            </AnonymousRoute>
+          }
+        />
         <Route
           path='/profile'
           element={
