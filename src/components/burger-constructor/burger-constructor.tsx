0e404d2e6ebffd27eed76eedd3458d843dfc17b0
Irina Ryazanskaya <ryazanskaya.irina@yandex.com>
2025-03-31 21:02:36 +0200

    Разрешила оформлять заказ только авторизованным пользователям

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,11 +1,13 @@
 import { FC, useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import {
   selectBurgerConstructorItems,
   selectBurgerOrder,
   createBurgerOrder,
   selectBurgerOrderRequestStatus,
-  clearBurgerOrderStatus
+  clearBurgerOrderStatus,
+  selectUser
 } from '@slices';
 import { BurgerConstructorUI } from '@ui';
 import { TConstructorIngredient } from '@utils-types';
@@ -13,12 +15,22 @@ import { useDispatch, useSelector } from '../../services/store';
 
 export const BurgerConstructor: FC = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const constructorItems = useSelector(selectBurgerConstructorItems);
   const orderRequestStatus = useSelector(selectBurgerOrderRequestStatus);
   const orderModalData = useSelector(selectBurgerOrder);
+  const user = useSelector(selectUser);
 
   const onOrderClick = () => {
-    if (!constructorItems.bun || orderRequestStatus === 'pending') return;
+    if (!user) {
+      navigate('/login');
+      return;
+    }
+
+    if (!constructorItems.bun || orderRequestStatus === 'pending') {
+      return;
+    }
 
     dispatch(
       createBurgerOrder([
