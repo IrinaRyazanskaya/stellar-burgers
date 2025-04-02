8cda55e9143f2a241c0b69d3c878ac00603e653b
Irina Ryazanskaya <ryazanskaya.irina@yandex.com>
2025-04-02 22:17:16 +0200

    Настроила jest

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const config: JestConfigWithTsJest = {
+  preset: 'ts-jest',
+  clearMocks: true,
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {}]
+  },
+  moduleNameMapper: {
+    '^@api$': '<rootDir>/src/utils/burger-api.ts',
+    '^@components$': '<rootDir>/src/components',
+    '^@pages$': '<rootDir>/src/pages',
+    '^@selectors$': '<rootDir>/src/services/selectors',
+    '^@slices$': '<rootDir>/src/services/slices',
+    '^@ui-pages$': '<rootDir>/src/components/ui/pages',
+    '^@ui$': '<rootDir>/src/components/ui',
+    '^@utils-types$': '<rootDir>/src/utils/types'
+  }
+};
+
+export default config;
