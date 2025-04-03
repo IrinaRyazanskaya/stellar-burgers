d543e563d26540eb1f59ae7f524280a44bc29955
Irina Ryazanskaya <ryazanskaya.irina@yandex.com>
2025-04-03 23:35:35 +0200

    Написала e2e тесты для модального окна с информацией об игредиенте

---
+++ cypress/e2e/burger-constructor/modal.cy.ts
@@ -0,0 +1,64 @@
+describe('Modal with ingredient details', function () {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://norma.nomoreparties.space/api/ingredients', {
+      fixture: 'ingredients.json'
+    }).as('getIngredients');
+
+    cy.visit('http://localhost:4000');
+
+    cy.wait('@getIngredients');
+
+    cy.get('[data-cy="burger-ingredients"]').should('be.visible');
+  });
+
+  it('should be opened when clicking on an ingredient', () => {
+    cy.contains('Флюоресцентная булка R2-D3').click();
+
+    cy.url().should(
+      'eq',
+      'http://localhost:4000/ingredients/643d69a5c3f7b9001cfa093d'
+    );
+
+    cy.get('[data-cy="modal"]').should('be.visible');
+
+    cy.get('[data-cy="modal"]').within(() => {
+      cy.contains('Флюоресцентная булка R2-D3').should('be.visible');
+
+      cy.contains('Белки').should('be.visible');
+      cy.contains('44').should('be.visible');
+
+      cy.contains('Жиры').should('be.visible');
+      cy.contains('26').should('be.visible');
+
+      cy.contains('Углеводы').should('be.visible');
+      cy.contains('85').should('be.visible');
+
+      cy.contains('Калории').should('be.visible');
+      cy.contains('643').should('be.visible');
+    });
+  });
+
+  it('should be closed when clicking close button', () => {
+    cy.contains('Флюоресцентная булка R2-D3').click();
+
+    cy.get('[data-cy="modal"]').should('be.visible');
+
+    cy.get('[data-cy="modal-close"]').click();
+
+    cy.get('[data-cy="modal"]').should('not.exist');
+
+    cy.url().should('eq', 'http://localhost:4000/');
+  });
+
+  it('should be closed when clicking outside the modal window', () => {
+    cy.contains('Флюоресцентная булка R2-D3').click();
+
+    cy.get('[data-cy="modal"]').should('be.visible');
+
+    cy.get('[data-cy="modal-overlay"]').click('topLeft', { force: true });
+
+    cy.get('[data-cy="modal"]').should('not.exist');
+
+    cy.url().should('eq', 'http://localhost:4000/');
+  });
+});
