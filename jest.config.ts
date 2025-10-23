c55ce6e19497b4313541ea17b633a4eb688846ac
IrinaRyazanskaya <ryazanskaya.irina@yandex.com>
2025-10-23 23:43:47 +0000

    Определила глобальные переменные в Jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,9 +4,17 @@ const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   clearMocks: true,
   testEnvironment: 'jsdom',
+
+  globals: {
+    __MODE__: 'unit-tests',
+    __BURGER_API_CLIENT__: 'mock',
+    __BURGER_API_BASE_URL__: ''
+  },
+
   transform: {
     '^.+\\.tsx?$': ['ts-jest', {}]
   },
+
   collectCoverageFrom: [
     '!src/**/*.d.ts',
     'src/**/*.{ts,tsx}',
