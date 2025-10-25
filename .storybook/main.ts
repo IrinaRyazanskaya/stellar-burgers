8aab606c31a6e11c539576c7ec4ad9ca8e119357
IrinaRyazanskaya <ryazanskaya.irina@yandex.com>
2025-10-25 21:20:37 +0000

    Исправила ошибки в Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,4 @@
+import { DefinePlugin } from "webpack";
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
@@ -5,7 +6,6 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
   ],
   framework: {
@@ -19,5 +19,32 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  swc: (config) => ({
+    ...config,
+    jsc: {
+      ...config.jsc,
+      transform: {
+        ...config.jsc?.transform,
+        react: {
+          ...config.jsc?.transform?.react,
+          runtime: "automatic",
+        },
+      },
+    },
+  }),
+  webpackFinal: async (webpackConfig) => {
+    webpackConfig.plugins = webpackConfig.plugins ?? [];
+
+    webpackConfig.plugins.push(
+      new DefinePlugin({
+        __MODE__: JSON.stringify(webpackConfig.mode),
+        __BURGER_API_CLIENT__: JSON.stringify("mock"),
+        __BURGER_API_BASE_URL__: JSON.stringify(""),
+      }),
+    );
+
+    return webpackConfig;
+  },
 };
+
 export default config;
