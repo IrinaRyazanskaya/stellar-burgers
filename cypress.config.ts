abb3c5df69f093f4196398f50dd9cbabb573a4ce
Irina Ryazanskaya <ryazanskaya.irina@yandex.com>
2025-10-26 06:14:56 +0500

    Настройка CI и адаптация к GitHub Pages (#4)
    * Починила E2E тесты
    
    * Удалила неиспользуемые пакеты
    
    * Добавила в игнор лишние файлы
    
    * Переписала README
    
    * Подготовила приложение к деплою на GitHub Pages
    
    * Сделала отдельную сборку с замоканным API

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "cypress";
-import dotenvPlugin from "cypress-dotenv";
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:4000",
-    setupNodeEvents(on, config) {
-      const updatedConfig = dotenvPlugin(config, null, true);
-      return updatedConfig;
+    env: {
+      BURGER_API_URL: "http://localhost:5000",
     },
   },
 });
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -7,79 +7,89 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
  * @param {{ mode?: 'development' | 'production' | 'none' }} argv
  * @returns {import('webpack').Configuration}
  */
-module.exports = (env, argv) => ({
-  entry: path.resolve(__dirname, "./src/index.tsx"),
+module.exports = (env = {}, argv = {}) => {
+  const mode = argv.mode || "development";
+  const apiClient = env.api === "real" ? "real" : "mock";
+  const apiBaseUrl = apiClient === "real" ? "http://localhost:5000" : "";
 
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".json", ".css", ".scss", ".png", ".svg", ".jpg"],
-  },
+  return {
+    entry: path.resolve(__dirname, "./src/index.tsx"),
 
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "./dist"),
-  },
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx", ".json", ".css", ".scss", ".png", ".svg", ".jpg"],
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.(ts)x?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "ts-loader",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "./dist"),
+    },
+
+    performance: {
+      hints: false,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
         },
-      },
-      {
-        test: /\.css$/,
-        exclude: /\.module\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.module\.css$/i,
-        exclude: /node_modules/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: {
-                namedExport: false,
-                exportLocalsConvention: "as-is",
+        {
+          test: /\.(ts)x?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "ts-loader",
+          },
+        },
+        {
+          test: /\.css$/,
+          exclude: /\.module\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.module\.css$/i,
+          exclude: /node_modules/,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: {
+                  namedExport: false,
+                  exportLocalsConvention: "as-is",
+                },
               },
             },
-          },
-        ],
-      },
-      {
-        test: /\.(jpg|jpeg|png|svg)$/,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2)$/,
-        type: "asset/resource",
-      },
+          ],
+        },
+        {
+          test: /\.(jpg|jpeg|png|svg)$/,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(woff|woff2)$/,
+          type: "asset/resource",
+        },
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+      new DefinePlugin({
+        __MODE__: JSON.stringify(mode),
+        __BURGER_API_CLIENT__: JSON.stringify(apiClient),
+        __BURGER_API_BASE_URL__: JSON.stringify(apiBaseUrl),
+      }),
     ],
-  },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-    new DefinePlugin({
-      __MODE__: JSON.stringify(argv.mode),
-      __BURGER_API_CLIENT__: JSON.stringify("mock"),
-      __BURGER_API_BASE_URL__: JSON.stringify(""),
-    }),
-  ],
-
-  devServer: {
-    port: 4000,
-    compress: true,
-    historyApiFallback: true,
-    static: path.join(__dirname, "./dist"),
-  },
-});
+    devServer: {
+      port: 4000,
+      compress: true,
+      historyApiFallback: true,
+      static: path.join(__dirname, "./dist"),
+    },
+  };
+};
