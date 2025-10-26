4d58aa46446a6d9baea808165e1be6f7fa46ad6f
IrinaRyazanskaya <ryazanskaya.irina@yandex.com>
2025-10-26 01:13:05 +0000

    Сделала отдельную сборку с замоканным API

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:4000",
+    env: {
+      BURGER_API_URL: "http://localhost:5000",
+    },
   },
 });
